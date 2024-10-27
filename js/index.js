c8ec182e183ff52cf5f4aec3936445d05976b0e4
SolracUihc <86658607+SolracUihc@users.noreply.github.com>
2024-10-27 20:50:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -172,21 +172,6 @@ export class App {
 
   }
 
-  // Function to update the positions of the boxes
-  updateBoxes() {
-    for (let i = 0; i < objects.length; i++) {
-      box = objects[i];
-    
-      // Move the box towards the camera
-      box.position.z += 0.1; // Adjust the speed as needed
-    
-      // Reset the box position if it is too close to the camera
-      if (box.position.z > 1) {
-        box.position.z = farZ; // Move the box back to the starting position
-      }
-    }
-  }
-
   /**
    * Handle window resize events to adjust camera and renderer.
    */
@@ -201,7 +186,6 @@ export class App {
    * Animate the scene and update hand controls.
    */
   animate() {
-    // updateBoxes(); // Animate moving box
     this.handControls?.animate(); // Animate hand controls if they exist
     ScenesManager.render(); // Render the scene
   }
