2d16e96e2802dbbdf3e12372d9221de3b2c3e5d7
SolracUihc <86658607+SolracUihc@users.noreply.github.com>
2024-11-19 21:37:07 +0800

    Update 3DAnimation.js

--- MIR/js/3DAnimation.js
+++ MIR/js/3DAnimation.js
@@ -47,8 +47,8 @@ export class GameAnimator {
 
         const box = new THREE.Mesh(geometry, material);
         box.position.set(
-            beatData.position.x * 4 - 0.5, // Scale and center x position
-            beatData.position.y * 4 + 1, // Scale and center y position
+            beatData.position.x * 4 - 2, // Scale and center x position
+            beatData.position.y * 4, // Scale and center y position
             -10 // Start far away
         );
 
